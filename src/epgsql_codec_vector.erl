a85b073489c25ba2f356841191816593e0429c19
Andrew Kane <andrew@ankane.org>
2026-01-29 16:43:02 -0800

    Improved code

--- src/epgsql_codec_vector.erl
+++ src/epgsql_codec_vector.erl
@@ -21,10 +21,10 @@ encode(Vec, halfvec, _) ->
     <<Dim:1/big-signed-unit:16, 0:1/big-signed-unit:16, Data/binary>>.
 
 decode(Bin, vector, _) ->
-    <<Dim:1/big-signed-unit:16, _Unused:1/big-signed-unit:16, Data:(Dim * 4)/binary>> = Bin,
+    <<Dim:1/big-signed-unit:16, 0:1/big-signed-unit:16, Data:(Dim * 4)/binary>> = Bin,
     [V || <<V:1/big-float-unit:32>> <= Data];
 decode(Bin, halfvec, _) ->
-    <<Dim:1/big-signed-unit:16, _Unused:1/big-signed-unit:16, Data:(Dim * 2)/binary>> = Bin,
+    <<Dim:1/big-signed-unit:16, 0:1/big-signed-unit:16, Data:(Dim * 2)/binary>> = Bin,
     [V || <<V:1/big-float-unit:16>> <= Data].
 
 decode_text(V, _, _) -> V.
